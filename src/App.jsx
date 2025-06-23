380a701dee5cb9de300159da032b1ad36eb47dac
Kevin <kevindc39@hotmail.com>
2025-06-23 11:42:24 +0200

    feature produits

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) =>
+        console.error("Erreur lors du chargement des produits:", err)
+      );
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container className="my-5 px-4" style={{ maxWidth: "1200px" }}>
+      <h1 className="mb-4 text-center">Fake Store</h1>
+      <Row className="g-4 justify-content-center">
+        {products.map((product) => (
+          <Col md={6} lg={4} xl={3} key={product.id}>
+            <Card className="h-100">
+              <div className="product-img-container">
+                <Card.Img
+                  variant="top"
+                  src={product.image}
+                  className="product-img"
+                />
+              </div>
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>{product.description}</Card.Text>
+                <div className="mt-auto">
+                  <h5>{product.price} $</h5>
+                  <Button variant="primary">Voir</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default App;
